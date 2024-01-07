73ab1f12ef1c5ad7403d98a2b78be44abf141555
cainytheslave <wajo432@gmail.com>
2024-01-07 22:45:04 +0100

    progress

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ extern crate opengl_graphics;
 extern crate piston;
 
 use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::{Button, EventLoop, MouseCursorEvent, PressEvent};
+use graphics::color::BLACK;
+use opengl_graphics::{GlGraphics, OpenGL, TextureSettings};
+use piston::{Button, EventLoop, MouseCursorEvent, PressEvent, window};
 use piston::event_loop::{Events, EventSettings};
 use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
 use piston::MouseButton::Left;
 use piston::window::WindowSettings;
 
+const RENDERER: OpenGL = OpenGL::V3_2;
 const COLUMNS: i32 = 7;
 const ROWS: i32 = 6;
 
@@ -51,6 +53,7 @@ pub struct App {
     gl: GlGraphics,
     game: Game,
     window_size: Size,
+    mouse_pos: Pos
 }
 
 impl App {
@@ -62,14 +65,16 @@ impl App {
         const DARK_BLUE: [f32; 4] = [0.0, 0.0, 0.0, 0.2];
         const YELLOW: [f32; 4] = [1.0, 1.0, 0.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const WHITE: [f32; 4] = [1.0; 4];
 
         self.window_size = (args.window_size[0], args.window_size[1]);
 
         let ((offset_x, offset_y), (board_size, _)) = self.get_dimensions();
 
         let col_width = board_size / COLUMNS as f64;
-
-
+        
+        let hover_column = self.get_mouse_column();
+        
         self.gl.draw(args.viewport(), |c, gl| {
             clear(GRAY, gl);
 
@@ -83,17 +88,37 @@ impl App {
                 if col % 2 == 0 {
                     rectangle(DARK_BLUE, [x, 0.0, col_width, board_size], t_matrix, gl);
                 }
+                
+                if let GameState::Running(player) = &self.game.state {
+                    let player_color = if *player == Player::Yellow { YELLOW } else { RED };
+ 
+                    for row in 0..ROWS {
+                        let y = board_size - row as f64 * col_width - col_width;
 
-                for row in (0..ROWS) {
-                    let y = board_size - row as f64 * col_width - col_width;
+                        if let Some(player) = &self.game.board[col as usize][row as usize] {
+                            let cell_color = if *player == Player::Yellow { YELLOW } else { RED };
+                            ellipse(cell_color, [x, y, col_width, col_width], t_matrix, gl);
+                        } else {
+                            ellipse(DARK_BLUE, [x, y, col_width, col_width], t_matrix, gl);
+                        }
+                    }
+                    
+                    match hover_column {
+                        Some(col) => {
+                            let rotated_matrix = t_matrix
+                                                .clone()
+                                                .trans(col as f64 * col_width + col_width / 2.0, col_width / 3.0)
+                                                .rot_deg(45.0);
 
-                    if let Some(player) = &self.game.board[col as usize][row as usize] {
-                        let player_color = if *player == Player::Yellow { YELLOW } else { RED };
-                        ellipse(player_color, [x, y, col_width, col_width], t_matrix, gl);
-                    } else {
-                        ellipse(DARK_BLUE, [x, y, col_width, col_width], t_matrix, gl);
+                            rectangle(player_color, [0.0, 0.0, col_width / 2.0, col_width / 2.0], rotated_matrix, gl);
+                        }
+                        None => (),
                     }
                 }
+
+                rectangle(WHITE, [0.0, 0.0, board_size, col_width / 1.5], t_matrix, gl);
+                line_from_to(graphics::color::BLACK, 2.0, [0.0, col_width / 1.5], [board_size, col_width / 1.5], t_matrix, gl);
+                //text(graphics::color::BLACK, 24, "FourWins", , t_matrix, gl);
             }
         });
     }
@@ -197,56 +222,73 @@ impl App {
         }
     }
 
-    fn handle_click(&mut self, pos: Pos) {
-        if let GameState::Running(player) = &self.game.state {
-            let ((ox, oy), (w, h)) = self.get_dimensions();
-            let pos = (pos.0 - ox, pos.1 - oy);
+    fn handle_click(&mut self) {
+        match &self.game.state {
+            GameState::Starting => {
+                self.game.state = GameState::Running(Player::Yellow);
+            },
 
-            let column_width = w / COLUMNS as f64;
-            let column_index = pos.0 / column_width;
-
-            if column_index < 0.0 || column_index >= COLUMNS as f64 { return; }
-            let column_index = column_index as usize;
-
-            for cell in self.game.board[column_index].iter_mut() {
-                match cell {
-                    None => {
-                        *cell = Some(player.clone());
-                        self.game.state = GameState::Running(player.op());
-                        self.handle_win();
-                        return;
-                    }
-                    Some(_) => {}
+            GameState::Running(player) => {
+                match self.get_mouse_column() {
+                    Some(column_index) => {
+                        for cell in self.game.board[column_index].iter_mut() {
+                            match cell {
+                                None => {
+                                    *cell = Some(player.clone());
+                                    self.game.state = GameState::Running(player.op());
+                                    self.handle_win();
+                                    return;
+                                }
+                                Some(_) => {}
+                            }
+                        }
+                    },
+                    None => return,
                 }
-            }
+
+            },
+
+            GameState::Win(_) | GameState::Draw => *self = App::initial(),
+        }
+    }
+    
+    fn get_mouse_column(&self) -> Option<usize> {
+        let ((ox, oy), (w, _h)) = self.get_dimensions();
+        let pos = (self.mouse_pos.0 - ox, self.mouse_pos.1 - oy);
+
+        let column_width = w / COLUMNS as f64;
+        let column_index = pos.0 / column_width;
+
+        if column_index < 0.0 || column_index >= COLUMNS as f64 {
+            return None;
+        }
+         
+        Some(column_index as usize)
+    }
+
+    fn initial() -> App {
+        App {
+            gl: GlGraphics::new(RENDERER),
+            game: Game {
+                board: vec![vec![None; 6]; 7],
+                state: GameState::Starting,
+            },
+            window_size: (0.0, 0.0),
+            mouse_pos: (0.0, 0.0),
         }
     }
 }
 
 fn main() {
-    let opengl = OpenGL::V3_2;
-
     let mut window: Window = WindowSettings::new("Four Wins", [800, 800])
-        .graphics_api(opengl)
+        .graphics_api(RENDERER)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-
-    let game = Game {
-        board: vec![vec![None; 6]; 7],
-        state: GameState::Running(Player::Yellow),
-    };
-
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        game,
-        window_size: (0.0, 0.0),
-    };
-
-    let mut cursor_pos = (0.0, 0.0);
-
+    let mut app = App::initial();
     let mut events = Events::new(EventSettings::new().lazy(true));
+
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
             app.render(&args);
@@ -258,10 +300,10 @@ fn main() {
 
         if let Some(Button::Mouse(button)) = e.press_args() {
             if button == Left {
-                app.handle_click(cursor_pos);
+                app.handle_click();
             }
         }
 
-        e.mouse_cursor(|pos| { cursor_pos = (pos[0], pos[1]) });
+        e.mouse_cursor(|pos| { app.mouse_pos = (pos[0], pos[1]) });
     }
 }
